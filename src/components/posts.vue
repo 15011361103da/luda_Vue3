44adbc1e32bbcd4379b1361102f5ff51f37cd78e
15011361103da <luda395353765@163.com>
2019-05-24 20:08:40 +0800

    请求接口

--- src/components/posts.vue
+++ src/components/posts.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-  import qs from 'qs'
+  /*import qs from 'qs'*/
     export default {
         name: "posts",
       data(){
@@ -18,13 +18,26 @@
         //form-data：？name=‘iwen’&age=18   注意：post参数为这种格式
         //x-www-form-urlencoded：｛name：‘iwen’，age=18｝
         //qs第三方插件可以转换参数类型
-        this.$axios.post("sxtstu/blueberrypai/login.php",qs.stringify({
+        /*this.$axios.post("sxtstu/blueberrypai/login.php",{
           user_id:'iwen@qq.com',
           password:'iwen123',
           verification_code:'crfvw'
-        }))
+        })
           .then(res => {
             console.log(res.data);
+          })
+          .catch(error => {
+            console.log(error);
+          })*/
+        var url = this.HOST+"/sug";
+        console.log(url)
+        this.$axios.post(url,{
+          code:'utf-8',
+          q:'ssss',
+          callback:'cb'
+        })
+          .then(res => {
+            console.log(res);
           })
           .catch(error => {
             console.log(error);
--- src/main.js
+++ src/main.js
@@ -3,10 +3,38 @@
 import Vue from 'vue'
 import App from './App'
 import Axios from 'axios'
+import qs from 'qs'
 
 Vue.prototype.$axios = Axios;
-Axios.defaults.baseURL = 'http://www.wwtliu.com/';
+Vue.prototype.HOST = "/api";
+//请求的地址前缀
+/*Axios.defaults.baseURL = 'http://baike.baidu.com/';*/
+Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
+// 添加请求拦截器
+Axios.interceptors.request.use(function (config) {
+  console.log("请求前"+config);
+  console.log(config);
+  //改变请求参数的数据类型：使用qs转换
+  /*if(config.method == 'post'){
+    config.data = qs.stringify(config.data);
+  }*/
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+Axios.interceptors.response.use(function (response) {
+  console.log("响应前"+response);
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
 
 Vue.config.productionTip = false
 
